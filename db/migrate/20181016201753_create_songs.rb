f62b1089c53e1d55d259f450a128f827806db4c3
Cristina Murillo <murillocam96@gmail.com>
2018-10-16 17:34:10 -0400

    Done.

---
+++ db/migrate/20181016201753_create_songs.rb
@@ -0,0 +1,11 @@
+class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.references :artist
+      t.references :genre
+
+      t.timestamps null: false
+    end
+  end
+end
